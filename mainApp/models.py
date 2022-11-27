188d7c2bd1d9079146cd3fa0c578eed72e48ad6b
Rhubert710 <73670513+Rhubert710@users.noreply.github.com>
2022-11-27 01:39:58 -0500

    added LikedDrink and Comment models

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+
+
+class LikedDrink (models.Model):
+
+    drinkId = models.CharField(max_length=200)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    liked = models.BooleanField()
+
+class Comment (models.Model):
+    
+    drinkId = models.CharField(max_length=200)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    text = models.CharField(max_length=401)
